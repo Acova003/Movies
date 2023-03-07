061fa241f52d5102a0fce6c78e7f5b723a6c1cfb
Amee <ameecova@yahoo.com>
2023-03-07 23:55:33 +0100

    Create class MovieApp

--- starter/src/test/java/com/javaunit3/springmvc/BestMovieService.java
+++ starter/src/test/java/com/javaunit3/springmvc/BestMovieService.java
@@ -1,11 +1,14 @@
+package com.javaunit3.springmvc;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 @Component
-public class BestMovieService {
+public class BestMovieService
+{
     @Autowired
-    public Movie Movie;
+    private Movie movie;
 
-    public Movie getBestMovie(){
+    public Movie getBestMovie() {
         return movie;
     }
 }
--- starter/src/test/java/com/javaunit3/springmvc/MovieApp.java
+++ starter/src/test/java/com/javaunit3/springmvc/MovieApp.java
@@ -1,13 +1,24 @@
 package com.javaunit3.springmvc;
 
+import org.springframework.beans.BeansException;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan
 public class MovieApp {
     public static void main(String[] args) {
-        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(MovieApp.class);
-        BestMovieService bestMovieService = applicationContext.getBean("bestMovieService", BestMovieService.class);
+        try (AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(MovieApp.class)) {
+            BestMovieService bestMovieService = applicationContext.getBean("bestMovieService", BestMovieService.class);
+
+            Movie bestMovie = bestMovieService.getBestMovie();
+
+            System.out.println("Title: " + bestMovie.getTitle());
+            System.out.println("Maturity rating: " + bestMovie.getMaturityRating());
+            System.out.println("Genre: " + bestMovie.getGenre());
+        } catch (BeansException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
 
     }
 }
