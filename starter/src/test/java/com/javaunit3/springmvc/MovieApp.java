50ea11fb730d1da0846a40232c5bebde336df8d7
Amee <ameecova@yahoo.com>
2023-03-08 18:08:17 +0100

    Create the best movie service

--- starter/src/test/java/com/javaunit3/springmvc/MovieApp.java
+++ starter/src/test/java/com/javaunit3/springmvc/MovieApp.java
@@ -1,24 +1,21 @@
 package com.javaunit3.springmvc;
 
-import org.springframework.beans.BeansException;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan
 public class MovieApp {
     public static void main(String[] args) {
-        try (AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(MovieApp.class)) {
-            BestMovieService bestMovieService = applicationContext.getBean("bestMovieService", BestMovieService.class);
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(MovieApp.class);
+        BestMovieService bestMovieService = applicationContext.getBean("bestMovieService", BestMovieService.class);
 
-            Movie bestMovie = bestMovieService.getBestMovie();
+        Movie bestMovie = bestMovieService.getBestMovie();
 
-            System.out.println("Title: " + bestMovie.getTitle());
-            System.out.println("Maturity rating: " + bestMovie.getMaturityRating());
-            System.out.println("Genre: " + bestMovie.getGenre());
-        } catch (BeansException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
+        System.out.println("Title: " + bestMovie.getTitle());
+        System.out.println("Maturity rating: " + bestMovie.getMaturityRating());
+        System.out.println("Genre: " + bestMovie.getGenre());
+
+        applicationContext.close();
 
     }
 }
