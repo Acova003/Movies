51adaffbc90ff0f7711bde11e7614c0272f12e8d
Amee <ameecova@yahoo.com>
2023-03-10 16:12:15 +0100

    Create set movie method

--- starter/src/test/java/com/javaunit3/springmvc/BestMovieService.java
+++ starter/src/test/java/com/javaunit3/springmvc/BestMovieService.java
@@ -6,12 +6,17 @@ import org.springframework.stereotype.Component;
 @Component
 public class BestMovieService
 {
-    @Autowired
     private Movie movie;
 
     public Movie getBestMovie() {
         return movie;
     }
+
+    @Autowired
+    public void setMovie(Movie movie){
+        this.movie = movie;
+    }
+
     @Autowired
     public BestMovieService(@Qualifier("batmanMovie")Movie movie) { 
         this.movie = movie;
