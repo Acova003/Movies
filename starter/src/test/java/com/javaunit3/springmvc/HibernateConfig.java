40fba1631b0b21c9e0743934f7dc5a71a7c89dde
Amee <ameecova@yahoo.com>
2023-03-20 16:08:59 +0100

    Create session factory bean in hibernate config

--- starter/src/test/java/com/javaunit3/springmvc/HibernateConfig.java
+++ starter/src/test/java/com/javaunit3/springmvc/HibernateConfig.java
@@ -1,10 +1,19 @@
 package com.javaunit3.springmvc;
 
 import org.hibernate.SessionFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class HibernateConfig {
-    SessionFactory factory = new org.hibernate.cfg.Configuration()
-        .configure("hibernate.cfg.xml")
-        .addAnnotatedClass(MovieEntity.class)
-        .buildSessionFactory();
+
+    @Bean
+    public SessionFactory getFactory() {
+        SessionFactory factory = new org.hibernate.cfg.Configuration()
+            .configure("hibernate.cfg.xml")
+            .addAnnotatedClass(MovieEntity.class)
+            .buildSessionFactory();
+        return factory;
+    }
+        
 }
